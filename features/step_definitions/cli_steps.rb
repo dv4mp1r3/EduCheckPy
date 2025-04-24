76f86c30a9a60b888fb1ecda27cb67dcf68b31f6
Guns-lingers <artem-kushnarev@mail.ru>
2025-04-25 00:14:04 +0700

    zxc

--- features/step_definitions/cli_steps.rb
+++ features/step_definitions/cli_steps.rb
@@ -1,14 +1,11 @@
 # features/step_definitions/cli_steps.rb
 require 'aruba/cucumber'
 
-Given('a blank slate') do
-  # пусто — арена чиста, а src уже rebased
-end
-
+Given('a blank slate') { }          # пусто — setup_aruba уже всё почистил
 When('I run {string}') do |cmd|
   run_command_and_stop(cmd, exit_timeout: 10)
 end
-
 Then('the output should contain {string}') do |expected|
   expect(last_command_started.output).to include(expected)
 end
+# Убираем свой Then('the exit status should be {int}')
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,12 @@
 # features/support/env.rb
 require 'aruba/cucumber'
+
 Aruba.configure do |config|
+  # timeout для команд, сек.
   config.exit_timeout = 10
 end
-Before { rebase(['src']) }
+
+Before do
+  # Подготовить aruba: очистить tmp, перейти в корень проекта
+  setup_aruba
+end
