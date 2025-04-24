aca57ac9118d64656d3866018d866850085d2819
Guns-lingers <artem-kushnarev@mail.ru>
2025-04-24 13:02:24 +0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -17,6 +17,7 @@ def cmd_echo(args):
     print(data.upper(), end="")
 
 def main():
+    test=0
     parser = argparse.ArgumentParser(prog="app.py")
     sub = parser.add_subparsers(dest="command")
 
