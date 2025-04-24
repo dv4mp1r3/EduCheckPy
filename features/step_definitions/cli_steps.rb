fe957f2db24f52f023ffc0628452358ef96e4bdf
Guns-lingers <artem-kushnarev@mail.ru>
2025-04-25 00:05:13 +0700

    Update cli_steps.rb

--- features/step_definitions/cli_steps.rb
+++ features/step_definitions/cli_steps.rb
@@ -1,16 +1,21 @@
-# Given('a blank slate') do
-#     # Здесь можно добавить очистку или инициализацию окружения, если необходимо
-#   end
-  
-#   When('I run {string}') do |command|
-#     run_command(command)
-#   end
-  
-# #   Then('the exit status should be {int}') do |status|
-# #     expect(last_command_started).to have_exit_status(status)
-# #   end
-  
-#   Then('the output should contain {string}') do |expected_output|
-#     expect(last_command_started).to have_output(/#{Regexp.escape(expected_output)}/)
-#   end
-  
+# features/step_definitions/cli_steps.rb
+require 'aruba/cucumber'
+
+Given('a blank slate') do
+  # ничего не делаем — aruba автоматически чистит рабочую директорию
+end
+
+When('I run {string}') do |cmd|
+  # запускаем команду и ждём её завершения
+  run_command_and_stop(cmd, exit_timeout: 10)
+end
+
+Then('the exit status should be {int}') do |status|
+  # проверяем код выхода последней команды
+  expect(last_command_started.exit_status).to eq(status)
+end
+
+Then('the output should contain {string}') do |expected|
+  # проверяем, что stdout содержит нужную подстроку
+  expect(last_command_started.output).to include(expected)
+end
