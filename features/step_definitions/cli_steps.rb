86a14b1570bf698d72bff14c6f0b7146a0125cb8
Guns-lingers <artem-kushnarev@mail.ru>
2025-04-25 01:38:44 +0700

    Update cli_steps.rb

--- features/step_definitions/cli_steps.rb
+++ features/step_definitions/cli_steps.rb
@@ -4,13 +4,6 @@ require 'aruba/cucumber'
 Given('a blank slate') { }
 
 When('I run {string}') do |cmd|
-  cmd.gsub!('src/', '') # Пути скорректированы после копирования
-  run_command_and_stop(cmd, exit_timeout: 15)
-end
-
-When('I run {string} with input:') do |cmd, input|
-  cmd.gsub!('src/', '')
-  run_command_and_stop(cmd, exit_timeout: 15) do |process|
-    process.write(input)
-  end
+  cmd.gsub!(/printf\s+"([^"]+)"/, 'echo -e "\1"')
+   run_command_and_stop(cmd, exit_timeout: 15)
 end
