3a83fb49c3748201e96c27d42b62d412d3b3da5a
Guns-lingers <artem-kushnarev@mail.ru>
2025-04-25 01:13:10 +0700

    Update cli_steps.rb

--- features/step_definitions/cli_steps.rb
+++ features/step_definitions/cli_steps.rb
@@ -4,16 +4,7 @@ require 'aruba/cucumber'
 Given('a blank slate') { }
 
 When('I run {string}') do |cmd|
-  # Убираем обработку конвейера через shell
-  cmd, input = cmd.split(' | ', 2)
-  
-  run_command_and_stop(cmd, exit_timeout: 15) do |process|
-    process.write(input) if input
-  end
-end
-
-Then('the output should contain {string}') do |expected|
-  # Нормализуем вывод: удаляем ANSI-коды и лишние пробелы
-  cleaned_output = last_command_started.output.gsub(/\e\[\d+m/, '').strip
-  expect(cleaned_output).to include(expected)
+  # Заменяем printf на встроенный метод Aruba для ввода
+  cmd.gsub!(/printf\s+"([^"]+)"/, 'echo -e "\1"')
+  run_command_and_stop(cmd, exit_timeout: 15)
 end
