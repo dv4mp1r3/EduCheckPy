aa9add8976c53c823772725a1933489d69a2df16
Guns-lingers <artem-kushnarev@mail.ru>
2025-04-24 15:07:33 +0700

    Update cli_steps.rb

--- features/step_definitions/cli_steps.rb
+++ features/step_definitions/cli_steps.rb
@@ -1,16 +1,16 @@
-Given('a blank slate') do
-    # Здесь можно добавить очистку или инициализацию окружения, если необходимо
-  end
-  
-  When('I run {string}') do |command|
-    run_command(command)
-  end
-  
-#   Then('the exit status should be {int}') do |status|
-#     expect(last_command_started).to have_exit_status(status)
+# Given('a blank slate') do
+#     # Здесь можно добавить очистку или инициализацию окружения, если необходимо
 #   end
   
-  Then('the output should contain {string}') do |expected_output|
-    expect(last_command_started).to have_output(/#{Regexp.escape(expected_output)}/)
-  end
+#   When('I run {string}') do |command|
+#     run_command(command)
+#   end
+  
+# #   Then('the exit status should be {int}') do |status|
+# #     expect(last_command_started).to have_exit_status(status)
+# #   end
+  
+#   Then('the output should contain {string}') do |expected_output|
+#     expect(last_command_started).to have_output(/#{Regexp.escape(expected_output)}/)
+#   end
   
