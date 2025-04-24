e569ab5848c820ccfb936d0c7b59059dcee332c0
Guns-lingers <artem-kushnarev@mail.ru>
2025-04-25 03:45:10 +0700

    Update cli_steps.rb

--- features/step_definitions/cli_steps.rb
+++ features/step_definitions/cli_steps.rb
@@ -10,8 +10,7 @@ end
 
 # ↓ новый шаг для обработки "with input:"
 When('I run {string} with input:') do |cmd, doc_string|
-  # 1) записываем переданный doc_string в tempfile
   write_file('stdin.txt', doc_string)
-  # 2) запускаем команду, перенаправляя stdin из файла
-  run_command_and_stop("#{cmd} < stdin.txt", exit_timeout: 15)
+  # оборачиваем вызов в shell, где < действительно работает
+  run_command_and_stop("sh -c \"#{cmd} < stdin.txt\"", exit_timeout: 15)
 end
