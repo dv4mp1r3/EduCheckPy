a3e8c78ca101dd373b903689a2de2f11a205adae
Guns-lingers <artem-kushnarev@mail.ru>
2025-04-25 00:56:04 +0700

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,11 +1,5 @@
 require 'aruba/cucumber'
 
 Aruba.configure do |config|
-  # вместо tmp/aruba — current directory (где лежит src/)
-  config.working_directory = '.'
   config.exit_timeout = 10
-end
-
-Before do
-  setup_aruba
 end
