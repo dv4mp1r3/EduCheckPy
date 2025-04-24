ce57351ccbdcd268b063b9b5d1a6cf22fec422e4
Guns-lingers <artem-kushnarev@mail.ru>
2025-04-25 00:09:19 +0700

    zxc

--- features/step_definitions/cli_steps.rb
+++ features/step_definitions/cli_steps.rb
@@ -2,20 +2,13 @@
 require 'aruba/cucumber'
 
 Given('a blank slate') do
-  # ничего не делаем — aruba автоматически чистит рабочую директорию
+  # пусто — арена чиста, а src уже rebased
 end
 
 When('I run {string}') do |cmd|
-  # запускаем команду и ждём её завершения
   run_command_and_stop(cmd, exit_timeout: 10)
 end
 
-Then('the exit status should be {int}') do |status|
-  # проверяем код выхода последней команды
-  expect(last_command_started.exit_status).to eq(status)
-end
-
 Then('the output should contain {string}') do |expected|
-  # проверяем, что stdout содержит нужную подстроку
   expect(last_command_started.output).to include(expected)
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,6 @@
+# features/support/env.rb
 require 'aruba/cucumber'
 Aruba.configure do |config|
   config.exit_timeout = 10
 end
+Before { rebase(['src']) }
