ec6753982a0fee6aac173c32de560ef7120dfc0b
Guns-lingers <artem-kushnarev@mail.ru>
2025-04-25 00:58:48 +0700

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,13 @@
+# features/support/env.rb
 require 'aruba/cucumber'
 
 Aruba.configure do |config|
-  config.exit_timeout = 10
+  config.working_directory = 'working_dir'  # Изолированная директория для тестов
+  config.exit_timeout = 15
+  config.activate_announcer = :always      # Включить отладку
+end
+
+Before do
+  # Копируем исходники в рабочую директорию Aruba
+  FileUtils.cp_r('src/', File.join(aruba.config.working_directory, 'src'))
 end
