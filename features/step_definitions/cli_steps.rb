14e73229c08afdc49fb7d9d800f2fb7c1eb7c957
Guns-lingers <artem-kushnarev@mail.ru>
2025-04-25 01:34:30 +0700

    Update cli_steps.rb

--- features/step_definitions/cli_steps.rb
+++ features/step_definitions/cli_steps.rb
@@ -4,6 +4,7 @@ require 'aruba/cucumber'
 Given('a blank slate') { }
 
 When('I run {string}') do |cmd|
+<<<<<<< HEAD
   cmd.gsub!('src/', '') # Пути скорректированы после копирования
   run_command_and_stop(cmd, exit_timeout: 15)
 end
@@ -13,4 +14,18 @@ When('I run {string} with input:') do |cmd, input|
   run_command_and_stop(cmd, exit_timeout: 15) do |process|
     process.write(input)
   end
+=======
+  # Убираем обработку конвейера через shell
+  cmd, input = cmd.split(' | ', 2)
+  
+  run_command_and_stop(cmd, exit_timeout: 15) do |process|
+    process.write(input) if input
+  end
+end
+
+Then('the output should contain {string}') do |expected|
+  # Нормализуем вывод: удаляем ANSI-коды и лишние пробелы
+  cleaned_output = last_command_started.output.gsub(/\e\[\d+m/, '').strip
+  expect(cleaned_output).to include(expected)
+>>>>>>> parent of 3a83fb4 (Update cli_steps.rb)
 end
