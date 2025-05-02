e35416923db52d186f5cfaa2f729e1197a1150a2
dv4mp1r3 <someone@somewhere>
2025-05-02 15:28:49 +0300

    docstring added

--- src/lab1.py
+++ src/lab1.py
@@ -1,12 +1,22 @@
+"""
+function for n*n calc
+"""
 import sys
-# comment
 
 
 def do_something_with_int_var(int_value: int) -> int:
+    """
+    result function
+
+    :param: int_value: value
+    """
     return int_value * int_value
 
 
 def main():
+    """
+    lab EP
+    """
     sys.stdout.write("main function test message")
     assert do_something_with_int_var(2) == 4, "correct assert"
     assert do_something_with_int_var(3) == 6, "wrong assert"
