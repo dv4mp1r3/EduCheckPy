2114003ea0222655bcca48e30fab15346e25b1c2
dv4mp1r3 <someone@somewhere>
2025-05-02 15:23:06 +0300

    pylint naming fix

--- src/lab1.py
+++ src/lab1.py
@@ -2,14 +2,14 @@ import sys
 # comment
 
 
-def foo(n: int) -> int:
-    return n * n
+def do_something_with_int_var(int_value: int) -> int:
+    return int_value * int_value
 
 
 def main():
     sys.stdout.write("main function test message")
-    assert foo(2) == 4, "correct assert"
-    assert foo(3) == 6, "wrong assert"
+    assert do_something_with_int_var(2) == 4, "correct assert"
+    assert do_something_with_int_var(3) == 6, "wrong assert"
 
 
 if __name__ == "__main__":
